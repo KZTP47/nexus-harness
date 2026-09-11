35a76dd6bf5c334a71b235877e85ac7078709dac
Naphal3000 <thewatcher3000@outlook.com>
2026-09-11 17:31:28 +0200

    Delete tests directory

--- tests/fixtures/playwright_unmodified_exact/playwright.config.cjs
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-
-module.exports = defineConfig({
-  workers: 1,
-  use: {
-    baseURL: process.env.NEXUS_TEST_EXACT_BASE_URL,
-    browserName: 'chromium',
-  },
-});
